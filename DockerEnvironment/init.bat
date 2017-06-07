c0c79a76268cb36841f79bad23e4b05fa0fef5c1
Elliot Salisbury <elliot.sal@gmail.com>
2017-06-07 14:24:14 +0100

    dockerfile for installing caffe, should now start to run CNN on webserver

--- DockerEnvironment/init.bat
+++ DockerEnvironment/init.bat
@@ -1,3 +1,4 @@
 docker-compose build
 docker-compose up -d
+docker-compose run web /usr/local/bin/python manage.py makemigrations FaceSwapApp
 docker-compose run web /usr/local/bin/python manage.py migrate
