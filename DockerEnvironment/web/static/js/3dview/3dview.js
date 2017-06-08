fd5dd18711a6be9dc8f2688613832e703d7e1fc3
Elliot Salisbury <elliot.sal@gmail.com>
2017-06-08 17:21:17 +0100

    its ugly right now, but it is working

---
+++ DockerEnvironment/web/static/js/3dview/3dview.js
@@ -0,0 +1,102 @@
+var container, camera, controls, scene, renderer;
+var currMesh;
+
+var IS_INITIALIZED = false;
+function init3d() {
+    if (!IS_INITIALIZED) {
+        IS_INITIALIZED = true;
+
+        //initialize the 3d environment
+        scene = new THREE.Scene();
+
+        container = document.getElementById('3dcanvas');
+        cW = 500; //container.clientWidth;
+        cH = 500; //container.clientHeight;
+
+        renderer = new THREE.WebGLRenderer();
+        renderer.setClearColor(0x444444);
+        renderer.setPixelRatio(window.devicePixelRatio);
+        renderer.setSize(cW, cH);
+
+
+        container.appendChild(renderer.domElement);
+
+        camera = new THREE.PerspectiveCamera(60, cW / cH, 1, 2000);
+        camera.position.x = 100;
+        camera.position.z = 300;
+
+
+        controls = new THREE.OrbitControls(camera, renderer.domElement);
+        controls.target = new THREE.Vector3(0, 00, 30);
+        controls.enableDamping = true;
+        controls.dampingFactor = 1.25;
+        controls.enableZoom = true;
+
+        var amblight = new THREE.AmbientLight(0x444444);
+        scene.add(amblight);
+
+        var dirLight = new THREE.DirectionalLight(0xffffff, 0.9);
+        dirLight.position.set(-100, 100, 200);
+        scene.add(dirLight);
+
+        window.addEventListener('resize', onWindowResize, false);
+    }
+}
+
+function onWindowResize() {
+    camera.left = container.clientWidth / - 2;
+    camera.right = container.clientWidth / 2;
+    camera.top = container.clientHeight / 2;
+    camera.bottom = container.clientHeight / - 2;
+
+    camera.updateProjectionMatrix();
+
+    renderer.setSize( container.clientWidth, container.clientHeight );
+}
+
+function animate() {
+    requestAnimationFrame( animate );
+    controls.update();
+    render();
+}
+
+function render() {
+    renderer.render( scene, camera );
+}
+
+function createMesh(verts, faces, uvs) {
+    scene.remove( currMesh );
+
+    var geometry = new THREE.Geometry();
+    geometry.dynamic = true;
+
+    for(var i=0; i<verts.length; i++) {
+        var vert = verts[i];
+        geometry.vertices.push(new THREE.Vector3( vert[0], vert[1], vert[2]));
+    }
+    for(var i=0; i<faces.length; i++) {
+        var face = faces[i];
+        geometry.faces.push(new THREE.Face3(face[0], face[1], face[2]));
+
+        // geometry.faceVertexUvs[0].push([    new THREE.Vector2(uvs[face[0]][0], 1-uvs[face[0]][1]),
+        //                                     new THREE.Vector2(uvs[face[1]][0], 1-uvs[face[1]][1]),
+        //                                     new THREE.Vector2(uvs[face[2]][0], 1-uvs[face[2]][1])]);
+    }
+
+    geometry.computeFaceNormals();
+
+    // var material = new THREE.MeshPhongMaterial( { map: THREE.ImageUtils.loadTexture('img/isomap.jpg') } );
+    var material = new THREE.MeshNormalMaterial();
+    currMesh = new THREE.Mesh( geometry, material );
+    scene.add( currMesh );
+}
+
+function updateMesh(verts) {
+    if (currMesh) {
+        for(var i=0; i<verts.length; i++) {
+            var vert = verts[i];
+            currMesh.geometry.vertices[i].set( vert[0], vert[1], vert[2]);
+        }
+        currMesh.geometry.verticesNeedUpdate = true;
+    }
+}
